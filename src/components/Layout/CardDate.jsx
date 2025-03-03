900cab78d747d7e91b4101f74125c59219853891
Najdorf <agustin.morro@gmail.com>
2025-03-03 13:44:07 -0300

    add styles

--- src/components/Layout/CardDate.jsx
+++ src/components/Layout/CardDate.jsx
@@ -3,7 +3,7 @@ const CardDate = () => {
     <div className="font-title relative text-balance text-sm lg:text-base flex w-full text-stone-500 border-b border-stone-400 pb-4 lg:pb-5">
       <div
         id="card-mask"
-        className="w-full h-full bg-zinc-600 absolute inset-0"
+        className="w-full h-full bg-stone-300 absolute inset-0 z-50"
       ></div>
       <span className="text-stone-400 self-center w-[10%] pl-2 md:pl-[3%]">
         Lorem
@@ -15,7 +15,7 @@ const CardDate = () => {
         </p>
       </div>
 
-      <div className="flex absolute right-6 gap-2 w-[20%] text-stone-400 text-nowrap justify-center items-center md:relative lg:gap-3">
+      <div className="flex absolute right-6 gap-2 z-20 w-[20%] text-stone-400 text-nowrap justify-center items-center md:relative lg:gap-3">
         <i className="bx bxs-cube-alt text-2xl xl:text-3xl text-red-500"></i>
         <p className="">20/04/2025</p>
       </div>
--- src/components/Layout/Contact.jsx
+++ src/components/Layout/Contact.jsx
@@ -42,70 +42,70 @@ const Contact = () => {
             </p>
           </div>
         </section>
-        <div className="w-[30%] h-[5vh] bg-stone-600 self-start mt-12"></div>
-        <div
-          id="third_section"
-          className="w-full relative font-title h-screen flex flex-col justify-start items-center md:justify-center lg:w-1/2 lg:self-start "
-        >
-        {/* <div className="absolute bg-stone-600 h-[300px] w-[300px]  bottom-12 -right-[700px]"></div>
-        <div className="absolute bg-red-500 h-[300px] w-[300px]  bottom-20 -right-[800px] z-40"></div>
-        <div className="absolute bg-stone-700 h-[300px] w-[300px]  bottom-32 -right-[900px] z-40"></div> */}
-
-          <article className="flex flex-col pt-16 justify-center items-center text-balance text-center gap-4 lg:pt-0 xl:gap-6">
-            <h6 className="font-text2 font-semibold text-6xl text-whiteCustom xl:text-7xl">
-              LET'S TALK
-            </h6>
-            <p className="text-stone-600 text-sm">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
-              Sapiente, et aspernatur? Accusantium fuga nulla
-            </p>
-          </article>
-          <div className="w-full max-w-md  mt-12 font-title text-sm flex flex-col justify-center lg:mt-6">
-            <div className="w-full max-w-md bg-gray-800 rounded-lg p-6">
-              <form
-                onSubmit={handleSubmit(onSubmit)}
-                className="flex flex-col w-full"
-              >
-                <input
-                  placeholder="Email address "
-                  className=" text-stone-600 border border-stone-700 rounded-md p-2 mb-4 bg-transparent focus:outline-none focus:ring-2 focus:ring-red-500 transition ease-in-out duration-150 placeholder:text-stone-600"
-                  type="email"
-                  autoComplete="off"
-                  {...register("email")}
-                />
-                <input
-                  placeholder="WhatsApp"
-                  autoComplete="off"
-                  className="text-stone-600 border border-stone-700 rounded-md p-2 mb-4 bg-transparent focus:outline-none focus:ring-2 focus:ring-red-500 transition ease-in-out duration-150 placeholder:text-stone-600"
-                  {...register("wttp")}
-                />
-                <textarea
-                  placeholder="Message"
-                  autoComplete="off"
-                  className=" text-stone-600 h-[120px] border border-stone-700 rounded-md p-2 mb-4 bg-transparent focus:outline-none focus:ring-2 focus:ring-red-500 transition ease-in-out duration-150 placeholder:text-stone-600"
-                  {...register("message")}
-                />
-                <button
-                  className="bg-gradient-to-tl border border-red-500 font-text2 text-red-500  py-2 px-4 rounded-md mt-4 transition ease-in-out duration-300 hover:bg-red-500 hover:text-whiteCustom"
-                  type="submit"
+        <div className="w-[30%] h-[5vh] rounded-r-sm self-start mt-12"></div>
+        <section className="w-full lg:flex lg:flex-row">
+          <div className="w-full relative font-title h-screen flex flex-col justify-start items-center md:justify-center lg:w-1/2 lg:self-start ">
+            <article className="flex flex-col pt-16 justify-center items-center text-balance text-center gap-4 lg:pt-0 xl:gap-6">
+              <h6 className="font-text2 font-semibold text-6xl text-whiteCustom xl:text-7xl">
+                LET'S TALK
+              </h6>
+              <p className="text-stone-600 text-sm">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
+                Sapiente, et aspernatur? Accusantium fuga nulla
+              </p>
+            </article>
+            <div className="w-full max-w-md  mt-12 font-title text-sm flex flex-col justify-center lg:mt-6">
+              <div className="w-full max-w-md bg-gray-800 rounded-lg p-6">
+                <form
+                  onSubmit={handleSubmit(onSubmit)}
+                  className="flex flex-col w-full"
                 >
-                  Send message
-                </button>
-              </form>
+                  <input
+                    placeholder="Email address "
+                    className=" text-stone-600 border border-stone-700 rounded-md p-2 mb-4 bg-transparent focus:outline-none focus:ring-2 focus:ring-red-500 transition ease-in-out duration-150 placeholder:text-stone-600"
+                    type="email"
+                    autoComplete="off"
+                    {...register("email")}
+                  />
+                  <input
+                    placeholder="WhatsApp"
+                    autoComplete="off"
+                    className="text-stone-600 border border-stone-700 rounded-md p-2 mb-4 bg-transparent focus:outline-none focus:ring-2 focus:ring-red-500 transition ease-in-out duration-150 placeholder:text-stone-600"
+                    {...register("wttp")}
+                  />
+                  <textarea
+                    placeholder="Message"
+                    autoComplete="off"
+                    className=" text-stone-600 h-[120px] border border-stone-700 rounded-md p-2 mb-4 bg-transparent focus:outline-none focus:ring-2 focus:ring-red-500 transition ease-in-out duration-150 placeholder:text-stone-600"
+                    {...register("message")}
+                  />
+                  <button
+                    className="bg-gradient-to-tl border border-red-500 font-text2 text-red-500  py-2 px-4 rounded-md mt-4 transition ease-in-out duration-300 hover:bg-red-500 hover:text-whiteCustom"
+                    type="submit"
+                  >
+                    Send message
+                  </button>
+                </form>
+              </div>
             </div>
+            <ul className="flex justify-center items-center gap-9 pt-8 text-stone-600 text-4xl lg:gap-16">
+              <li>
+                <i className="bx bxl-instagram hover:text-red-500 duration-500 hover:scale-105 hover:cursor-pointer"></i>
+              </li>
+              <li>
+                <i className="bx bxl-facebook hover:text-red-500 duration-500 hover:scale-105 hover:cursor-pointer"></i>
+              </li>
+              <li>
+                <i className="bx bxl-twitter hover:text-red-500 duration-500 hover:scale-105 hover:cursor-pointer"></i>
+              </li>
+            </ul>
           </div>
-          <ul className="flex justify-center items-center gap-9 pt-8 text-stone-600 text-4xl lg:gap-16">
-            <li>
-              <i className="bx bxl-instagram hover:text-red-500 duration-500 hover:scale-105 hover:cursor-pointer"></i>
-            </li>
-            <li>
-              <i className="bx bxl-facebook hover:text-red-500 duration-500 hover:scale-105 hover:cursor-pointer"></i>
-            </li>
-            <li>
-              <i className="bx bxl-twitter hover:text-red-500 duration-500 hover:scale-105 hover:cursor-pointer"></i>
-            </li>
-          </ul>
-        </div>
+          <div className="hidden lg:w-1/2 lg:flex items-start justify-end relative mt-12 ">
+            <div className="w-[300px] h-[300px]  bg-stone-700 z-50 absolute mr-32 mt-12"></div>
+            <div className="w-[300px] h-[300px] bg-stone-600 z-40 absolute mr-44 mt-32"></div>
+            <div className="w-[300px] h-[300px] bg-red-500 z-50 absolute"></div>
+          </div>
+        </section>
       </div>
     </>
   );
